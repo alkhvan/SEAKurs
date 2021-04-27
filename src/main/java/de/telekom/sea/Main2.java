704a5d4e88f9fca7dad00786a1914e5c5d32235c
Aleksandra Khvan <aleksandra.klochko@t-systems.com>
2021-04-27 12:15:38 +0300

    new Person was added; SeminarApp was updated

--- src/main/java/de/telekom/sea/Main2.java
+++ src/main/java/de/telekom/sea/Main2.java
@@ -4,9 +4,8 @@ import de.telekom.sea.seminar.Person;
 
 public class Main2 {
     public static void main(String[] args) {
-        new Person();
-        System.out.println("Hello World!");
+
         SeminarApp app = new SeminarApp();
-        app.run(args);
+        app.run (args,"Alberta");
     }
 }
--- src/main/java/de/telekom/sea/seminar/Person.java
+++ src/main/java/de/telekom/sea/seminar/Person.java
@@ -1,6 +1,19 @@
 package de.telekom.sea.seminar;
 
 public class Person {
-   public Object parent;
+ //  public Object parent;
+   private String name;
+
+
+    public  Person(String name){
+       this.name=name;
+    }
+
+
+
+   public String getName() {
+      return name;
+   }
+
 
 }
